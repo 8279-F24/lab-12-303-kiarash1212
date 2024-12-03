cfb86be2a3cccda6614040e462912b168abf3c92
kia <etem0009@algonquinlive.com>
2024-12-02 22:12:40 -0500

    done

---
+++ converter.py
@@ -0,0 +1,27 @@
+Morse_codes = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----'}
+
+
+theInput = input().lower()
+
+theMorses = []
+for char in theInput:
+    
+    if char == ' ':theMorses.append('       ')
+    else:
+        if char.capitalize() not in Morse_codes.keys():continue
+        theMorses += list(' '.join(Morse_codes[char.capitalize()]))
+        theMorses.append('   ')
+    
+print(theMorses)
